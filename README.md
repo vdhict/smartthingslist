eb4a5624c24541989cdad1dac89e4c2cee6612ab
Sander van der Heijden <sheijden@vdh-ict.nl>
2023-12-06 18:13:36 +0100

    change script output to json. adjust README.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# smartthingslist
-Small Script to list all device ID's from Smartthings
+# Smartthingslist
+
+This is a small script to list Smartthings devices from a certain location, filtering on a name or part of the name.
+
+I created this script as a companion to my home-infrastructure IaaC attempt to fully automate the build of my home infrastructure.
+
+Usage:
+
+```shell
+smartthingslist.py [-h] --location LOCATION --key KEY --devicename DEVICENAME
+```
+
+--location: The location name of the Smartthings location
+--key: Smartthings API key
+--devicename: (part of) the name of the device to find
+
+Output is a json formatted list of device labels and their ID's.
--- smartthingslist.py
+++ smartthingslist.py
@@ -2,18 +2,21 @@ import asyncio
 import aiohttp
 import argparse
 from pysmartthings import SmartThings
+import json
 
 parser = argparse.ArgumentParser(description='List devices in a SmartThings location.')
 
 # Adding arguments
 parser.add_argument('--location', required=True, help='Name of the location')
 parser.add_argument('--key', required=True, help='API key')
-
+parser.add_argument('--devicename', required=True, help='(part of) The name of the device. If part of the name is given, all devices matching the searchstring will be listed.')
+                    
 # Parse arguments
 args = parser.parse_args()
 
 location_name = args.location
 key = args.key
+name = args.devicename
 
 async def main():
     async with aiohttp.ClientSession() as session:
@@ -22,14 +25,18 @@ async def main():
         if len(locations) == 0:
             print("no locations for that API key")
         location_id = next(loc.location_id for loc in locations if loc.name == location_name)
-        
-        print ("location_id: ", location_id)
 
         devices = await api.devices()
         if len(devices) == 0:
             print("no devices for that API key")
+        device_data = []
         for device in devices:
-            if 'NUC' in device.label:
-                print("found device: ", device.device_id, device.name, device.label)
+            if name in device.label:
+                # Add device info to the list as a dictionary
+                device_data.append({"name": device.label, "device_id": device.device_id})
+
+        # Convert the list of dictionaries to a JSON string
+        json_output = json.dumps(device_data, indent=4)
+        print(json_output)  # Print the JSON string
             
 asyncio.run(main())
